11b30458ecf81261d9e284e98a9540d43afa7d5f
ESI BUCEO <esi-buceo@outlook.com>
2023-10-04 13:52:50 -0300

    Primer commit e implementacion de BD

---
+++ baseDeDatos.sql
@@ -0,0 +1,13 @@
+create database escrito;
+
+use escrito;
+
+create table tareas(
+	id int not null primary key auto_increment,
+    titulo varchar(40) not null,
+    contenido varchar (100) not null,
+    autor varchar (30) not null,
+    created_at timestamp,
+    updated_at datetime,
+    deleted_at datetime
+);
