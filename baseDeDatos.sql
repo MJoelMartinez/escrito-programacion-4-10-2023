4a0864e6b6aef5b10f6104aff6a0150aca81bd92
Maykol Martínez <mjoelmartinez320@gmail.com>
2023-10-04 14:48:57 -0300

    Modificacion de BD

--- baseDeDatos.sql
+++ baseDeDatos.sql
@@ -6,8 +6,11 @@ create table tareas(
 	id int not null primary key auto_increment,
     titulo varchar(40) not null,
     contenido varchar (100) not null,
+    estado varchar(20) not null,
     autor varchar (30) not null,
     created_at timestamp,
     updated_at datetime,
     deleted_at datetime
 );
+
+drop table tareas;
